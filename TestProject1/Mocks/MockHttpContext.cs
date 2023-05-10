3ae8307d4fcd405d18cac11440d7df5d2e567965
Rafael Maestre Del Río <a20madera@iesgrancapitan.org>
2023-05-10 13:06:31 +0200

    Subida de test unitarios e inicio de mockeo

---
+++ TestProject1/Mocks/MockHttpContext.cs
@@ -0,0 +1,28 @@
+﻿using api_neo_nasa.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Newtonsoft.Json;
+
+namespace TestProject1.Mocks
+{
+    public class MockHttpContext
+    {
+        [Test]
+        public async Task SetHttpContext_ParameterIsNull_ReturnsOkObjectResult()
+        {
+            string days = "2";
+            var controllerMock = new Mock<AsteroidsController>();
+            string jsonContent = File.ReadAllText("C:\\Users\\RAFAEL.MAESTRE\\source\\repos\\PruebaNET\\TestProject1\\Mocks\\Resources\\ResponseBody.json");
+            var jsonResponse = JsonConvert.DeserializeObject(jsonContent);
+            var response = new OkObjectResult(jsonResponse);
+            controllerMock.Setup(c => c.Get(days)).ReturnsAsync(response);
+
+            var micontroller = controllerMock.Object;
+
+            var result = await micontroller.Get(days);
+
+            Assert.That(result, Is.InstanceOf<OkObjectResult>());
+
+        }
+    }
+}
--- api-neo-nasa/Controllers/AsteroidsController.cs
+++ api-neo-nasa/Controllers/AsteroidsController.cs
@@ -16,54 +16,75 @@ namespace api_neo_nasa.Controllers
         private readonly string DEMO_KEY = "DEMO_KEY";
 
         [HttpGet]
-        public async Task<IActionResult> Get([Required(ErrorMessage ="El campo days es requerido")]
-                                                int days)
+        public virtual async Task<IActionResult> Get([Required(ErrorMessage ="El campo days es requerido")]
+                                                string days)
         // Im using DataAnnotation to validate if the user use this method without {days} parameter.
         {
-            string url = GenerateParametersUrl(days, PERSONAL_KEY);
-
-            HttpClient client = new();
-            var response = await client.GetAsync(url);
-            if (response.IsSuccessStatusCode)
+            try
             {
-                // If the response returns a 200 code i take de data and deserialize it into my model.
-                var jsonBody = await response.Content.ReadAsStringAsync();
-                var deserializedData = JsonConvert.DeserializeObject<NEOModel>(jsonBody);
+                CheckParameter(days); // This method is used to check the posibly errors and throw exception
+                string url = GenerateParametersUrl(days,PERSONAL_KEY);
 
-                List<NEODTO> AsteroidsList = new(); // This List is used to save all AsteroidsDTO that were gona declare
-                foreach (var item in deserializedData.Objects)
+                HttpClient client = new();
+                var response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
                 {
-                    foreach (var dia in item.Value)
+                    // If the response returns a 200 code i take de data and deserialize it into my model.
+                    var jsonBody = await response.Content.ReadAsStringAsync();
+                    var deserializedData = JsonConvert.DeserializeObject<NEOModel>(jsonBody);
+                    List<NEODTO> AsteroidsList = new(); // This List is used to save all AsteroidsDTO that were gona declare
+                    foreach (var item in deserializedData.Objects)
                     {
-                        if (dia.Dangerous) // We only gonna display to the user the top 3 dangerous new earth objects order by descending mean
+                        foreach (var day in item.Value)
                         {
-                            AsteroidsList.Add(new NEODTO
+                            if (day.Dangerous) // We only gonna display to the user the top 3 dangerous new earth objects order by descending mean
                             {
-                                Name = dia.Name,
-                                Mean = dia.Mean,
-                                Speed = dia.Speed,
-                                Date = dia.Date,
-                                Planet = dia.Planet
-                            });
+                                AsteroidsList.Add(new NEODTO
+                                {
+                                    Name = day.Name,
+                                    Mean = day.Mean,
+                                    Speed = day.Speed,
+                                    Date = day.Date,
+                                    Planet = day.Planet
+                                });
 
+                            }
                         }
                     }
+                    return Ok(AsteroidsList.OrderByDescending(x => x.Mean)
+                        .Take(3));
                 }
-                return Ok(AsteroidsList.OrderByDescending(x => x.Mean)
-                    .Take(3));
+                else
+                {
+                    return BadRequest(response);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return BadRequest(response);
+                return BadRequest(ex.Message);
             }
         }
 
 
-        private static string GenerateParametersUrl(int days, string KEY)
+        private static string GenerateParametersUrl(string days, string KEY)
         {
             string startDate = $"?start_date={DateTime.Today:yyyy-MM-dd}";
-            string endDate = $"&end_date={DateTime.Today.AddDays(days):yyyy-MM-dd}";
+            string endDate = $"&end_date={DateTime.Today.AddDays(Int32.Parse(days)):yyyy-MM-dd}";
             return $"{API_URL}{startDate}{endDate}&api_key={KEY}";
+        }
+
+        private static void CheckParameter(string days)
+        {
+            int parsedDay = int.Parse(days);
+            if (days is null)
+            {
+                throw new ArgumentNullException(nameof(days));
+            }
+            else if (parsedDay < 1 || parsedDay > 7)
+            {
+                throw new ArgumentOutOfRangeException(nameof(days));
+            }
 
         }
     }
